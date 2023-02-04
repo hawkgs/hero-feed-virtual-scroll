127abeed7ad1272a2aebe65b0285d0da5cccbc33
hawkgs <hawk.dvp@gmail.com>
2023-02-04 23:36:52 +0200

    feat: add hero-message component

--- src/app/api/generator.ts
+++ src/app/api/generator.ts
@@ -1,33 +1,38 @@
-import { uniqueNamesGenerator, adjectives, colors, names } from 'unique-names-generator';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  names,
+} from 'unique-names-generator';
 import { LoremIpsum } from 'lorem-ipsum';
 import { HeroMessage } from '../model/hero-message';
 
 const randomNum = (min: number, max: number) => {
   return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 export const generateMessages = (size: number): HeroMessage[] => {
   const lorem = new LoremIpsum({
     wordsPerSentence: {
       max: 10,
-      min: 4
-    }
+      min: 4,
+    },
   });
 
-  return new Array(size).fill(null).map(
-   () => ({
-     name: uniqueNamesGenerator({
-      dictionaries: [adjectives, colors, names],
-      length: 2,
-      style: 'capital',
-      separator: ' '
-    }),
-     date: new Date(randomNum(2015, 2023), randomNum(0, 11), randomNum(1, 30)),
-     message: lorem.generateSentences(1),
-     tags: new Array(randomNum(0, 1) ? randomNum(0, 5) : 0)
-      .fill(null)
-      .map(() => uniqueNamesGenerator({ dictionaries: [colors], length: 1 })),
-   })
-  ).sort((a, b) => b.date.getTime() - a.date.getTime());
-}
-
+  return new Array(size)
+    .fill(null)
+    .map(() => ({
+      name: uniqueNamesGenerator({
+        dictionaries: [adjectives, colors, names],
+        length: 2,
+        style: 'capital',
+        separator: ' ',
+      }),
+      date: new Date(randomNum(2015, 2023), randomNum(0, 11), randomNum(1, 30)),
+      message: lorem.generateSentences(1),
+      tags: new Array(randomNum(0, 1) ? randomNum(0, 5) : 0)
+        .fill(null)
+        .map(() => uniqueNamesGenerator({ dictionaries: [colors], length: 1 })),
+    }))
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+};
--- src/app/hero-message/hero-message.component.ts
+++ src/app/hero-message/hero-message.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeroMessage } from '../model/hero-message';
 
 @Component({
   selector: 'app-hero-message',
   templateUrl: './hero-message.component.html',
-  styleUrls: ['./hero-message.component.scss']
+  styleUrls: ['./hero-message.component.scss'],
 })
 export class HeroMessageComponent {
+  @Input() message!: HeroMessage;
 
+  get initials() {
+    return this.message.name.split(' ').map((name: string) => name[0]);
+  }
 }
