ba410a17cab29261bcf80149685aef84a05cb932
hawkgs <hawk.dvp@gmail.com>
2023-02-04 14:04:46 +0200

    feat: add cdk-virtual-scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
