3a3569c820032defa2de41a27961cd90391af9ec
hawkgs <hawk.dvp@gmail.com>
2023-02-05 12:12:05 +0200

    feat: style hero-message

--- src/app/hero-message/hero-message.component.ts
+++ src/app/hero-message/hero-message.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HeroMessage } from '../model/hero-message';
 
+const expand = (char: number) => {
+  const min = 65;
+  const max = 90;
+  const range = max - min;
+  const pos = char - min;
+
+  return Math.round(255 * (pos / range));
+};
+
 @Component({
   selector: 'app-hero-message',
   templateUrl: './hero-message.component.html',
@@ -14,5 +23,12 @@ export class HeroMessageComponent {
       .split(' ')
       .map((name: string) => name[0])
       .join('');
+  }
+
+  get color() {
+    const c = this.message.name.toUpperCase();
+    return `rgb(${expand(c.charCodeAt(0))}, ${expand(
+      c.charCodeAt(1),
+    )}, ${expand(c.charCodeAt(2))})`;
   }
 }
